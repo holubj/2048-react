1038e0503a277dc725029e5fff11ac94c1f56e81
Jakub Holub <holubjakub@me.com>
2019-08-25 02:30:39 +0200

    Updates

---
+++ precache-manifest.cdcc70c79ec560651fed1a6bd5ae60e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426f3f4b49feff5160072de7a39549bd",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "3e7c0a9956c864a7d6f6",
+    "url": "/2048-react/static/js/2.04c5ac77.chunk.js"
+  },
+  {
+    "revision": "aefee9eeb4983ca5792d",
+    "url": "/2048-react/static/js/main.efe2d2e9.chunk.js"
+  },
+  {
+    "revision": "fa09d4da666bf9bdd93e",
+    "url": "/2048-react/static/js/runtime~main.2290ad1c.js"
+  }
+]);
